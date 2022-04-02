b1c7e37656796c9dea7d928dc8dfb0e06fb5bee1
Tergel Munkhdelger <tergelm@gmail.com>
2022-04-02 15:33:50 +0800

    earth setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import glowUrl from "./glow.svg";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
-function App() {
+interface AppProps {}
+//
+const App: React.FC<AppProps> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="hero">
+        <div></div>
+        <img alt="hero-glow" src={glowUrl} className="hero-glow" />
+        <div className="globe-canvas-container">
+          <Canvas className="webgl-canvas" fallback={null}>
+            <OrbitControls />
+            <ambientLight />
+            <directionalLight
+              color="#FAF5FF"
+              position={[0, 2, 4]}
+              intensity={4}
+            />
+            <directionalLight
+              color="#A855F7"
+              position={[0, -5, -4]}
+              intensity={1}
+            />
+            <mesh rotation={[0, Math.PI / 2, Math.PI / 4]}>
+              <sphereGeometry args={[2, 32, 16]} />
+              <meshStandardMaterial color="#1d2460" />
+            </mesh>
+          </Canvas>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
